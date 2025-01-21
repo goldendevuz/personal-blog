83482b9604501c2cbecfddbeb80ccf2d32b7fc15
goldendevuz <goldendevuz@gmail.com>
2025-01-21 21:20:49 +0500

    21.20.21.01.2025

--- config/settings.py
+++ config/settings.py
@@ -82,15 +82,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'mariadb',
-    'USER':'gold',
-    'PASSWORD':'1920',
-    'HOST':'localhost',
-    'PORT':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
